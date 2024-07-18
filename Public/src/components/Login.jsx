f178d2abf3a6184b05e6d600d93cc32a2b83ee1d
Jay Vishwakarma <100530367+jay-3012@users.noreply.github.com>
2024-07-18 23:41:39 +0530

    Update Login.jsx

--- Public/src/components/Login.jsx
+++ Public/src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:3000/user/login", userInfo)
+      .post("https://ecommerce-nine-wine-88.vercel.app/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
