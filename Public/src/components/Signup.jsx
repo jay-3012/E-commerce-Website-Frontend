00977d65039f6334c90508a09af9b5dbdc329c9a
Jay Vishwakarma <100530367+jay-3012@users.noreply.github.com>
2024-07-18 23:56:04 +0530

    Update Signup.jsx

--- Public/src/components/Signup.jsx
+++ Public/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       email: data.email,
       password: data.password,
     };
-    await axios.post("https://ecommerce-nine-wine-88.vercel.app/user/signup", userInfo)
+    await axios.post("https://ecommerce-zhu2.onrender.com/user/signup", userInfo)
     .then((res) => {
         console.log(res.data);
         if (res.data) {
